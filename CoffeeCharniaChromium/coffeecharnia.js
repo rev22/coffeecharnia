39eb9ba272aaf2768858461f3cd0d4120f329298
Michele Bini <michele.bini@gmail.com>
2015-02-25 19:40:15 +0100

    Bump version

--- CoffeeCharniaChromium/coffeecharnia.js
+++ CoffeeCharniaChromium/coffeecharnia.js
@@ -7,7 +7,7 @@ window.coffeecharnia = {
     codeLogUrl: "http://localhost/cgi-bin/coffeecharnialog"
   },
   pkgInfo: {
-    version: "CoffeeCharnia 0.3.52",
+    version: "CoffeeCharnia 0.3.53",
     description: "Reflective CoffeeScript Console",
     copyright: "Copyright (c) 2014, 2015 Michele Bini",
     license: "GPL3"
